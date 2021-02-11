8210539058f5861b1f40ae87ae0ee642fa572951
boba2fett <boba2002fett@gmail.com>
2021-02-11 21:29:30 +0100

    rm comments

--- blockchain.py
+++ blockchain.py
@@ -15,33 +15,21 @@ class Blockchain:
         self.chain = []
         self.nodes = set()
 
-        # Create the genesis block
+        # genesis block
         self.new_block(previous_hash='1', proof=100)
 
     def register_node(self, address):
-        """
-        Add a new node to the list of nodes
-
-        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
-        """
 
         parsed_url = urlparse(address)
         if parsed_url.netloc:
             self.nodes.add(parsed_url.netloc)
         elif parsed_url.path:
-            # Accepts an URL without scheme like '192.168.0.5:5000'.
             self.nodes.add(parsed_url.path)
         else:
             raise ValueError('Invalid URL')
 
 
     def valid_chain(self, chain):
-        """
-        Determine if a given blockchain is valid
-
-        :param chain: A blockchain
-        :return: True if valid, False if not
-        """
 
         last_block = chain[0]
         current_index = 1
@@ -66,12 +54,6 @@ class Blockchain:
         return True
 
     def resolve_conflicts(self):
-        """
-        This is our consensus algorithm, it resolves conflicts
-        by replacing our chain with the longest one in the network.
-
-        :return: True if our chain was replaced, False if not
-        """
 
         neighbours = self.nodes
         new_chain = None
@@ -100,13 +82,6 @@ class Blockchain:
         return False
 
     def new_block(self, proof, previous_hash):
-        """
-        Create a new Block in the Blockchain
-
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
-        :return: New Block
-        """
 
         block = {
             'index': len(self.chain) + 1,
@@ -123,14 +98,7 @@ class Blockchain:
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
 
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
-        """
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -145,26 +113,12 @@ class Blockchain:
 
     @staticmethod
     def hash(block):
-        """
-        Creates a SHA-256 hash of a Block
-
-        :param block: Block
-        """
 
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_block):
-        """
-        Simple Proof of Work Algorithm:
-
-         - Find a number p' such that hash(pp') contains leading 4 zeroes
-         - Where p is the previous proof, and p' is the new proof
-         
-        :param last_block: <dict> last Block
-        :return: <int>
-        """
 
         last_proof = last_block['proof']
         last_hash = self.hash(last_block)
@@ -177,15 +131,6 @@ class Blockchain:
 
     @staticmethod
     def valid_proof(last_proof, proof, last_hash):
-        """
-        Validates the Proof
-
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :param last_hash: <str> The hash of the Previous Block
-        :return: <bool> True if correct, False if not.
-
-        """
 
         guess = f'{last_proof}{proof}{last_hash}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
@@ -235,9 +180,8 @@ def mine():
 def new_transaction():
     values = request.get_json()
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
+    requiredJsonContents = ['sender', 'recipient', 'amount']
+    if not all(key in values for key in requiredJsonContents):
         return 'Missing values', 400
 
     # Create a new Transaction
@@ -293,7 +237,10 @@ def consensus():
 
 @app.route('/nodes', methods=['GET'])
 def all_nodes():
-    return jsonify(list(blockchain.nodes)), 200
+    response = {
+        'nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
